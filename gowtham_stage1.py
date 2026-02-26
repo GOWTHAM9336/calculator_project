dc9e8fbebe299a3814b9805b052b2385cf12afc5
GOWTHAM9336 <gowthammani1162005@gmail.com>
2026-02-26 18:07:40 +0530

    Update gowtham_stage1.py

---
+++ gowtham_stage1.py
@@ -0,0 +1,29 @@
+# Stage 1: Basic Calculator
+
+# Taking input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+# Using if-elif-else
+if operator == "+":
+    result = num1 + num2
+    print("Result =", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result =", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result =", result)
+
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed")
+    else:
+        result = num1 / num2
+        print("Result =", result)
+
+else:
+    print("Invalid operator")
